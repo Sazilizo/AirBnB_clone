85abd2ea28509bced6bf65b057154f75ca1e2fc9
root <lizosazi@gmail.com>
2023-12-18 17:05:58 +0000

    named arguments

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,57 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+"""Defines all common attributes/methods for other classes
+"""
 import uuid
 from datetime import datetime
-"""This module is containes a class defining the base of the AirBnB clone
-"""
+import models
+
 
 class BaseModel:
-	"""
-	a class defining the baseodel of the airBnB clone
-	"""
-	def __init__(self, id, created_at, updated_at):
-		self.id = uuid.uuid4()
-		self.created_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-		self.updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-	def __str__(self):
-		print("{} {} {}".format(self.__class__.__name__, self.id, self.__dict__))
+    """Base class for all models"""
 
-	def save(self):
-		self.updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-	def to_dict(self):
-		"""keys/values of the instance"""
-        	new_dict = self.__dict__.copy()
-        	new_dict['updated_at'] = self.updated_at.isoformat()
-        	new_dict['created_at'] = self.created_at.isoformat()
-        	new_dict['__class__'] = self.__class__.__name__
-		
-		return new_dict
+    def __init__(self, *args, **kwargs):
+        """Initializes the Base.
+        Args:
+            *args: list of arguments
+            **kwargs: named arguments
+        """
+        if kwargs:
+            datetime_isoformat = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], datetime_isoformat)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], datetime_isoformat)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a readable string representation
+        of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the instance attribute updated_at
+        with the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns all
+        keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+
+	return new_dict
